236f517b9a84f3221cf58b6df158b403400c4d2d
vanesamunoz <vanesamunoz@vanesas-mbp.wireless-1x.unc.edu>
2019-08-29 18:02:06 -0400

    edit jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,16 +7,10 @@ public class A1Jedi {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		
-		
 		
 		int numberofitems = scan.nextInt();
 		
-		//Matched the name with the price
-		
 		String[] products = new String[numberofitems];
-		
 		double[] priceofproducts = new double[numberofitems];
 		
 		for(int i = 0; i < numberofitems; i++) {
@@ -25,25 +19,18 @@ public class A1Jedi {
 		}
 		
 		
-		// Put the names with their firstname and lastname into an array
-		
 		int numberofpeople = scan.nextInt();
 		
+		int quantity = 0;
+		String product = "";
+
 		String[] personfirstname = new String[numberofpeople];
 		
 		String[] personlastname = new String[numberofpeople];
 		
-		
-		
 		int[] quantityofitemsbought = new int[numberofitems];
 		
 		int[] quantityofpersons = new int[numberofitems];
-		
-	
-		
-		
-		// for loop goes through the number of people (3)
-		// puts the first name and last name into corresponding arrays
 		
 		for(int a = 0; a < numberofpeople; a++) {
 			String firstname = scan.next();
@@ -53,56 +40,38 @@ public class A1Jedi {
 			personlastname[a] = lastname;
 			
 			int itemsbought = scan.nextInt();
-			
-			
-			
-			// for loop that goes through the number of items (differs slightly from people)
-			
+			boolean[] jack = new boolean[numberofitems];
 			for(int z = 0; z < itemsbought; z++) {
 				
-				int quantity = scan.nextInt();	
-				String nameofproduct = scan.next();
-				
-		// for loop goes through the number of items (6)
+				quantity = scan.nextInt();	
+				product = scan.next();
 				
 				for(int v = 0; v < numberofitems; v++) {
-		
-		// if the product contains the name of product then the array of quantity of 
-		// items bought changes as well as the quantity of persons
-					
-					if(products[v].equals(nameofproduct)) {
+					if(products[v].equals(product)) {
 						quantityofitemsbought[v] += quantity;
-						quantityofpersons[v] += 1;
+						jack[v] = true;
 					}
-					
-					
 				}
-				
-			
 			}
-			 
 			
-			
+			for(int t = 0; t < jack.length; t++) {
+				if(jack[t]) {
+					quantityofpersons[t]++;
+				}
+			}
 		}
-		
 		for(int y = 0; y < numberofitems; y++) {
-			if(quantityofitemsbought[y] == 0 ) {
-				System.out.println("No" + " " + "customers bought " + products[y]);
+			if(quantityofpersons[y] == 0 ) {
+				System.out.println("No customers bought " + products[y]);
 			} else {
 		
-			System.out.println(quantityofpersons[y] + " " + "customers bought " + quantityofitemsbought[y] + " " + products[y]);
-			
+			System.out.println(quantityofpersons[y] + " customers bought " + quantityofitemsbought[y] + " " + products[y]);
 		}
-		}
-		
-		
-	scan.close();
-		
-	
-		
-	
-	
+			scan.close();
+		}	
 	}
+	
+}
 		
-	}
+	
 
